b6102982fae9d40d980f7c078a14e57d35870884
Hamza Khalid <33630482+hamzakalid@users.noreply.github.com>
2021-01-27 11:50:26 +0300

    UpLoad The Project

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# web-project
+
+# Store Site
+
+this is  a store site show some of our products and helps the customer to contact as
+
+team :
+
+1. Hamza khaled 
+2. Ammar Yasser
+3. Nabeel Abdorab
+4. Khaled Abdulqawi
+5. Ahmed Faiz
---
+++ js/app.js
@@ -0,0 +1,79 @@
+
+$(document).ready(function(){
+    $('.loader').fadeOut();
+    $('.product-owl  .owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        autoplay:true,
+        // responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                // nav:true
+            },
+            600:{
+                items:3,
+                // nav:false
+            },
+            1000:{
+                items:5,
+                // nav:true,
+                // loop:false
+            }
+        }
+     });
+     
+
+     $('.testimonials .owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        autoplay:true,
+        items:1
+    });
+
+
+     $('.menu-icon').on('click',function(){
+        $('.menu').toggleClass('show');
+     });
+
+     //Mixup start
+     
+     AOS.init({
+        duration: 600,
+        easing: 'ease-in-sine',
+        dataAosOnce:true
+     });
+     $( "#Onavbar" ).click(function( e ) {
+        e.stopPropagation();
+        $(this).removeClass('show');
+
+      }).on('click','a',function(e) {
+        e.stopPropagation();
+   });
+   //when you click on the menu item
+   $('nav .groups .menu ul li a').on('click',function(){
+       $(this).addClass('active').parent().siblings().children().removeClass('active')
+   })
+
+   //Start with scroll top action
+   $(window).scroll(function () { 
+       var winSrollTop =$(this).scrollTop();
+       $('section').each(function(){
+            if(winSrollTop >= $(this).offset().top - 80){
+                // console.log();
+                $('nav .groups .menu ul li a[href=#'+$(this).attr('id')+']').addClass('active').parent().siblings().children().removeClass('active')
+            }
+       });
+   });
+
+   $('.theme-btns li').on('click', function () {
+       if($(this).data('color')!= 'blue'){
+            var colorName ="css/style."+$(this).data('color')+".color.css";
+            $('.theme').attr('href', colorName);
+       }else{
+           $('.theme').removeAttr('href');
+       }
+   });
+});
